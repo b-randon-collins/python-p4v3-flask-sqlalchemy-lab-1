2d8c3a73bb13ba23ffcc08d94e72a41e2226b743
Brandon Collins <brandonc503@yahoo.com>
2024-09-21 10:50:45 -0400

    Adds view to get earthquakes matching a minimum magnitude value

--- server/app.py
+++ server/app.py
@@ -30,5 +30,18 @@ def earthquake_by_id(id):
     else:
         return make_response(jsonify({'message': f'Earthquake {id} not found.'}), 404)
 
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    with db.session() as session: 
+        earthquakes = session.query(Earthquake).filter(Earthquake.magnitude >= magnitude).all()
+
+        quake_list = [quake.to_dict() for quake in earthquakes]
+        body = {
+            "count": len(quake_list),
+            "quakes": quake_list
+        }
+
+    return make_response(jsonify(body), 200)
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
